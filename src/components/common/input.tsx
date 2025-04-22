c5338017de37ceb774ce06275b68ea03628003d4
김태욱 <idididsjkim@gmali.com>
2025-04-22 13:50:45 +0900

    feat: input 컴포넌트 구현

--- src/components/common/input.tsx
+++ src/components/common/input.tsx
@@ -1,4 +1,42 @@
-import React from "react";
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-  error?: string;
+import React from 'react'
+
+interface InputProps {
+  value: string
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  type?: string
+  placeholder?: string
+  disabled?: boolean
+  readOnly?: boolean
+  error?: string
+  className?: string
 }
+
+const Input: React.FC<InputProps> = ({
+  value,
+  onChange,
+  type = 'text',
+  placeholder = '',
+  disabled = false,
+  readOnly = false,
+  error,
+  className = '',
+}) => {
+  return (
+    <div className="flex flex-col">
+      <input
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+        disabled={disabled}
+        readOnly={readOnly}
+        className={`border rounded p-2 ${
+          error ? 'border-red-500' : 'border-gray-300'
+        } ${className}`}
+      />
+      {error && <span className="text-red-500 text-xs mt-1">{error}</span>}
+    </div>
+  )
+}
+
+export default Input
