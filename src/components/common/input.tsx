c5e1a412621204f3ab690ce25271c77556ba6455
김태욱 <idididsjkim@gmali.com>
2025-04-23 22:43:24 +0900

    feat: input.tx 생성하고 interface InputProps 이동

--- src/components/common/input.tsx
+++ src/components/common/input.tsx
@@ -2,21 +2,7 @@
 
 import React from 'react'
 import styles from './input.module.css'
-
-interface InputProps {
-  value: string
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void
-  type?: string
-  placeholder?: string
-  disabled?: boolean
-  readOnly?: boolean
-  error?: string
-  className?: string
-  padding?: string
-  height?: string
-  icon?: React.ReactNode
-  width?: string
-}
+import InputProps from '@/types/common/input'
 
 const Input: React.FC<InputProps> = ({
   value,
