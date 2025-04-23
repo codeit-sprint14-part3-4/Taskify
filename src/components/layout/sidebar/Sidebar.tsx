fe54009a80b19d4393fc2e79d638a38b079354d8
parkchanyoung0710 <call4892@naver.com>
2025-04-23 12:03:19 +0900

    img에서 next/image로 변경

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -1,20 +1,29 @@
 import React from 'react'
 import Link from 'next/link'
+import Image from 'next/image'
 import styles from './Sidebar.module.css'
 
 export default function Sidebar() {
   return (
     <div className={styles.sidebar}>
       <Link href="/" className={styles.logo}>
-        <img
+        <Image
           src="/assets/icon/logo-icon.svg"
           alt="Taskify Icon"
           className={styles.logoIcon}
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: '100%', height: 'auto' }}
         />
-        <img
+        <Image
           src="/assets/icon/logo-title.svg"
           alt="Taskify Title"
           className={styles.logoTitle}
+          width={0}
+          height={0}
+          sizes="100vw"
+          style={{ width: '100%', height: 'auto' }}
         />
       </Link>
 
@@ -22,7 +31,13 @@ export default function Sidebar() {
         <li className={styles.menuItem}>
           <span className={`${styles.link} text-xs-semibold`}>Dash Boards</span>
           <button className={styles.addButton}>
-            <img src="/assets/image/add-box.svg" alt="Add" />
+            <Image
+              src="/assets/image/add-box.svg"
+              alt="Add"
+              className=""
+              width={20}
+              height={20}
+            />
           </button>
         </li>
       </ul>
