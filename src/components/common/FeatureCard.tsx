3a2ec2a238e4342f0bac489c9423f6ce918e4f51
김태욱 <idididsjkim@gmali.com>
2025-04-25 01:52:53 +0900

    feat: FeatyreCard 컴포넌트를 만들어서 landingpage에 있는 카드 편의성 추가

---
+++ src/components/common/FeatureCard.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import Image from 'next/image'
+import styles from './featureCard.module.css'
+
+interface FeatureCardProps {
+  imageSrc: string
+  title: string
+  description: string
+  width?: number
+  height?: number
+}
+
+const FeatureCard: React.FC<FeatureCardProps> = ({
+  imageSrc,
+  title,
+  description,
+  width,
+  height,
+}) => {
+  return (
+    <div className={`${styles.featureCardFrame}`}>
+      <div className={`${styles.cardboxtop}`}>
+        <Image
+          src={imageSrc}
+          alt={title}
+          width={width || 100}
+          height={height || 100}
+        />
+      </div>
+      <div className={`${styles.cardboxbottom}`}>
+        <h3 className={`${styles.cardh3}`}>{title}</h3>
+        <p className={`${styles.cardtext}`}>{description}</p>
+      </div>
+    </div>
+  )
+}
+
+export default FeatureCard
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import Gnb from '@/components/layout/gnb/Gnb'
 import Image from 'next/image'
 import styles from './index.module.css'
 import Button from '@/components/common/button/Button'
+import FeatureCard from '@/components/common/featureCard'
 
 export default function Home() {
   return (
@@ -26,6 +27,13 @@ export default function Home() {
         </section>
 
         <section className={`${styles.pointer1}  flex-col`}>
+          <Image
+            src="/assets/image/landing1.svg"
+            alt="landingpage image"
+            width={594}
+            height={497.49}
+            className={`${styles.landingimage1}`}
+          />
           <span className={`${styles.pointer1text}`}>Point1</span>
           <div className={`${styles.pointer1maintext} text-left`}>
             <div>일의 우선 순위를</div>
@@ -34,6 +42,13 @@ export default function Home() {
         </section>
 
         <section className={`${styles.pointer2} flex flex-col text-left`}>
+          <Image
+            src="/assets/image/landing2.svg"
+            alt="landingpage image"
+            width={436}
+            height={502}
+            className={`${styles.landingimage2}`}
+          />
           <div className={`${styles.pointer2box}`}>
             <span className={`${styles.pointer2text}`}>Point2</span>
             <div className={`${styles.pointer2maintext}`}>
@@ -49,10 +64,31 @@ export default function Home() {
           </span>
         </section>
 
-        <section className="flex flex-row text">
-          <div></div>
+        <section className={`${styles.cardListWrapper}`}>
+          <FeatureCard
+            imageSrc="/assets/image/landing3.svg"
+            title="대시보드 설정"
+            description="대시보드 사진과 이름을 변경할 수 있어요."
+            width={300}
+            height={123.87}
+          />
+          <FeatureCard
+            imageSrc="/assets/image/landing4.svg"
+            title="초대"
+            description="새로운 팀원을 초대할 수 있어요."
+            width={300}
+            height={230.81}
+          />
+          <FeatureCard
+            imageSrc="/assets/image/landing5.svg"
+            title="구성원"
+            description="구성원을 초대하고 내보낼 수 있어요."
+            width={300}
+            height={195.48}
+          />
         </section>
       </div>
+      <footer></footer>
     </div>
   )
 }
