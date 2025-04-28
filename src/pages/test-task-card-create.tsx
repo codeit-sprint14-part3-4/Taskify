ab56e06f95aeb928af39d3e7ef2384d2ca8a6249
huiseong29 <tobestar29@naver.com>
2025-04-28 21:51:27 +0900

    refactor: test 파일 삭제

--- src/pages/test-task-card-create.tsx
+++
@@ -1,9 +0,0 @@
-import TaskCardCreateModal from '@/components/domain/modals/TaskCardCreateModal'
-
-export default function TestTaskCardCreatePage() {
-  return (
-    <div className="flex justify-center items-center h-screen bg-[var(--gray-FAFAFA)]">
-      <TaskCardCreateModal />
-    </div>
-  )
-}
