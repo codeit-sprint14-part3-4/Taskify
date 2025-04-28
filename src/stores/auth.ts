2d5e5fad9811972ddd7266f887996b84ab456c7e
ramong26 <rlatndus21049@gmail.com>
2025-04-28 23:45:29 +0900

    refactor: 토큰 드  ㄹ대로

--- src/stores/auth.ts
+++ src/stores/auth.ts
@@ -1,5 +1,6 @@
+// 토큰 코드
 import { create } from 'zustand'
-import { persist } from 'zustand/middleware'
+import { persist, createJSONStorage } from 'zustand/middleware' // persist를 사용하면 상태를 로컬 스토리지나 세션 스토리지에 자동으로 저장하고, 페이지를 새로 고침해도 상태가 유지됨 ( 하지만 URL을 자동으로 처리하지는 않기 때문에, 리다이렉트 URL이 없는 경우에는 상태(예: accessToken)만 저장되고, 어디로 리다이렉트할지에 대한 정보는 zustand나 다른 상태 관리 도구에서 관리되지 않음)
 
 type AuthState = {
   accessToken: string | null
@@ -10,21 +11,21 @@ type AuthState = {
 export const useAuthStore = create<AuthState>()(
   persist(
     (set) => ({
-      accessToken: null, // 기본값 설정
-      setAccessToken: (token: string) => set({ accessToken: token }), // setAccessToken 정의
-      clearAccessToken: () => set({ accessToken: null }), // clearAccessToken 정의
+      accessToken: null,
+      setAccessToken: (token) => {
+        set({ accessToken: token })
+      },
+      clearAccessToken: () => {
+        set({ accessToken: null })
+      },
     }),
     {
-      name: 'my-session-store',
-      storage: {
-        getItem: (name) => {
-          const stored = sessionStorage.getItem(name)
-          return stored ? JSON.parse(stored) : null
-        },
-        setItem: (name, value) =>
-          sessionStorage.setItem(name, JSON.stringify(value)),
-        removeItem: (name) => sessionStorage.removeItem(name),
-      },
+      name: 'auth-storage', //저장소 이름
+      storage:
+        typeof window !== 'undefined' &&
+        window.location.hostname === 'localhost'
+          ? createJSONStorage(() => localStorage) // 개발환경에서는 로컬스토리지 사용
+          : createJSONStorage(() => sessionStorage), // 배포 환경에서는 세션스토리지 사용
     }
   )
 )
