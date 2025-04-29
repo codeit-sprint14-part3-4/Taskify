f0fdca7747fd355c4eea2cab6277b74717ecd697
huiseong29 <tobestar29@naver.com>
2025-04-29 21:38:34 +0900

    feature: 태그 x 버튼 삭제 기능 추가

--- src/components/common/tag/Tag.tsx
+++ src/components/common/tag/Tag.tsx
@@ -1,14 +1,39 @@
+import Image from 'next/image'
 import styles from './Tag.module.css'
 import type { TagProps } from '@/types/common/tag'
 
-export default function Tag({ label, color }: TagProps) {
+interface ExtendedTagProps extends TagProps {
+  isDeletable?: boolean
+  onDelete?: () => void
+}
+
+export default function Tag({
+  label,
+  color,
+  isDeletable = false,
+  onDelete,
+}: ExtendedTagProps) {
   return (
     <div
       className={`${styles.tag} ${
         styles[color as keyof typeof styles]
-      } text-md-regular`}
+      } text-md-regular relative`}
     >
       {label}
+      {isDeletable && onDelete && (
+        <button
+          type="button"
+          onClick={onDelete}
+          className="ml-[0.2rem] flex items-center justify-center"
+        >
+          <Image
+            src="/assets/image/close.svg"
+            alt="태그 삭제"
+            width={12}
+            height={12}
+          />
+        </button>
+      )}
     </div>
   )
 }
