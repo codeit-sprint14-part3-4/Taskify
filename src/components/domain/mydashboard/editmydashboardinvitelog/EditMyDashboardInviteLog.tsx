e47c7c7c9402208f8a36f1fb6c28df8eba38d305
jihoon kwak <kwakjihoon1001@gmail.com>
2025-04-30 10:14:24 +0900

    feat: dashboard edit 페이지 UI 개발

--- src/components/domain/mydashboard/editmydashboardinvitelog/EditMyDashboardInviteLog.tsx
+++ src/components/domain/mydashboard/editmydashboardinvitelog/EditMyDashboardInviteLog.tsx
@@ -1,3 +1,108 @@
+import styles from './editMyDashboardInviteLog.module.css'
+import CommonButton from '@/components/common/commonbutton/CommonButton'
+
 export default function EditMyDashboardInviteLog() {
-  return <div>초대 기록 관리 하기 컴포넌트</div>
+  return (
+    <>
+      <div className={styles.edit_invite_container}>
+        <div className={styles.edit_invite_header_flex_container}>
+          <div className={`text-2xl-bold`}>초대 내역</div>
+          <div className={styles.edit_invite_header_flex_container}>
+            <div>1 페이지 중 1</div>
+            {/*페이지네이션 버튼*/}
+            <CommonButton
+              variant="primary"
+              padding="0.8rem 1.6rem"
+              isActive={true}
+              className={`text-md-medium`}
+            >
+              초대하기
+            </CommonButton>
+          </div>
+        </div>
+        <div className={`${styles.edit_invite_email_header} text-lg-regular`}>
+          이메일
+        </div>
+        <div className={styles.invite_cancle_container}>
+          <div>
+            <div className={`${styles.edit_invite_email} text-lg-regular`}>
+              codeitA@codeit.com
+            </div>
+          </div>
+          <CommonButton
+            variant="secondary"
+            padding="0.4rem 2.95rem"
+            isActive={true}
+            className={`${styles.edit_cancel_button} text-md-medium`}
+          >
+            취소
+          </CommonButton>
+        </div>
+
+        <div className={styles.invite_cancle_container}>
+          <div>
+            <div className={`${styles.edit_invite_email} text-lg-regular`}>
+              codeitB@codeit.com
+            </div>
+          </div>
+          <CommonButton
+            variant="secondary"
+            padding="0.4rem 2.95rem"
+            isActive={true}
+            className={`${styles.edit_cancel_button} text-md-medium`}
+          >
+            취소
+          </CommonButton>
+        </div>
+
+        <div className={styles.invite_cancle_container}>
+          <div>
+            <div className={`${styles.edit_invite_email} text-lg-regular`}>
+              codeitC@codeit.com
+            </div>
+          </div>
+          <CommonButton
+            variant="secondary"
+            padding="0.4rem 2.95rem"
+            isActive={true}
+            className={`${styles.edit_cancel_button} text-md-medium`}
+          >
+            취소
+          </CommonButton>
+        </div>
+
+        <div className={styles.invite_cancle_container}>
+          <div>
+            <div className={`${styles.edit_invite_email} text-lg-regular`}>
+              codeitD@codeit.com
+            </div>
+          </div>
+          <CommonButton
+            variant="secondary"
+            padding="0.4rem 2.95rem"
+            isActive={true}
+            className={`${styles.edit_cancel_button} text-md-medium`}
+          >
+            취소
+          </CommonButton>
+        </div>
+
+        <div className={styles.invite_cancle_container}>
+          <div>
+            <div className={`${styles.edit_invite_email} text-lg-regular`}>
+              codeitE@codeit.com
+            </div>
+          </div>
+          <CommonButton
+            variant="secondary"
+            padding="0.4rem 2.95rem"
+            isActive={true}
+            className={`${styles.edit_cancel_button} text-md-medium`}
+          >
+            취소
+          </CommonButton>
+        </div>
+      </div>
+    </>
+  )
 }
--- src/pages/dashboard/[id]/edit/index.tsx
+++ src/pages/dashboard/[id]/edit/index.tsx
@@ -1,12 +1,24 @@
 import EditMyDashboardAttribute from '@/components/domain/mydashboard/editmydashboardattribute/EditMyDashboardAttribute'
 import EditMyDashboardMember from '@/components/domain/mydashboard/editmydashboardmember/EditMyDashboardMember'
 import EditMyDashboardInviteLog from '@/components/domain/mydashboard/editmydashboardinvitelog/EditMyDashboardInviteLog'
+import styles from './edit.module.css'
 export default function EditPage() {
+  const handleLouterPrev = () => {
+    console.log('이전 페이지로 이동하는 로직 작성하기')
+  }
+
+  const handleDashboardDelete = () => {
+    console.log('대시보드 삭제하는 로직 작성하기')
+  }
   return (
     <>
-      <EditMyDashboardAttribute />
-      <EditMyDashboardMember />
-      <EditMyDashboardInviteLog />
+      <button onClick={handleLouterPrev}>돌아가기</button>
+      <div className={styles.edit_container}>
+        <EditMyDashboardAttribute />
+        <EditMyDashboardMember />
+        <EditMyDashboardInviteLog />
+      </div>
+      <button onClick={handleDashboardDelete}>대시보드 삭제하기</button>
     </>
   )
 }
