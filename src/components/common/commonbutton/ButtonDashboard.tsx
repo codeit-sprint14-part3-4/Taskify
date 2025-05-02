30770de256296c4d5b6aa14ec0e7940c50b54a0a
ramong26 <rlatndus21049@gmail.com>
2025-05-02 23:39:19 +0900

    refactor: 대시보드버튼 수정

--- src/components/common/commonbutton/ButtonDashboard.tsx
+++ src/components/common/commonbutton/ButtonDashboard.tsx
@@ -45,11 +45,11 @@ export default function ButtonDashboard({
     <div>
       <button
         onClick={onClick}
-        className={clsx(styles.buttonWrapper)}
+        className={clsx(styles.buttonWrapper, buttonClass, innerClass)}
         style={style}
         {...rest}
       >
-        <div className={clsx(innerClass, buttonClass)}>
+        <div className={styles.buttonContent}>
           {prefix && <span className={styles.icon}>{prefix}</span>}
           {children && <span>{children}</span>}
           {suffix && <span className={styles.icon}>{suffix}</span>}
