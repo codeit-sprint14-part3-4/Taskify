27c3337686e3341ef1ae83380181492c2c6a668d
huiseong29 <tobestar29@naver.com>
2025-05-03 20:43:07 +0900

    feature: 사이드바 api 연결

--- src/components/layout/sidebar/Sidebar.tsx
+++ src/components/layout/sidebar/Sidebar.tsx
@@ -1,14 +1,34 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
 import styles from './sidebar.module.css'
 import DashboardCreateModal from '@/components/domain/modals/dashboardCreateModal/DashboardCreateModal'
+import { Dashboard } from '@/types/api/dashboards'
+import { dashboardsService } from '@/api/services/dashboardsServices'
 
-export default function Sidebar() {
+export default function Sidebar({
+  currentDashboardId,
+}: {
+  currentDashboardId: number
+}) {
   const [isModalOpen, setIsModalOpen] = useState(false)
+  const [dashboardList, setDashboardList] = useState<Dashboard[]>([])
 
   const handleOpenModal = () => setIsModalOpen(true)
   const handleCloseModal = () => setIsModalOpen(false)
+
+  const getDashboardList = async () => {
+    try {
+      const res = await dashboardsService.getDashboards('infiniteScroll')
+      setDashboardList(res.dashboards)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+
+  useEffect(() => {
+    getDashboardList()
+  }, [])
 
   return (
     <div className={styles.sidebar}>
@@ -43,7 +63,38 @@ export default function Sidebar() {
             />
           </button>
         </li>
+        {dashboardList &&
+          dashboardList.length &&
+          dashboardList.map((dashboard) => (
+            <li key={dashboard.id}>
+              <Link
+                className={`flex items-center gap-[16px] mb-[8px] px-[12px] py-[12px] rounded-[4px] ${
+                  currentDashboardId === dashboard.id ? 'bg-[#F1EFFD]' : ''
+                }`}
+                href={`/dashboard/${dashboard.id}`}
+              >
+                <div
+                  className="w-[8px] h-[8px] rounded-full"
+                  style={{ backgroundColor: dashboard.color }}
+                ></div>
+                <div className="flex items-center gap-[6px]">
+                  <div className="text-2lg-medium text-[#787486]">
+                    {dashboard.title}
+                  </div>
+                  {dashboard.createdByMe && (
+                    <Image
+                      src="/assets/icon/crown.svg"
+                      alt="왕관"
+                      width={20}
+                      height={16}
+                    />
+                  )}
+                </div>
+              </Link>
+            </li>
+          ))}
       </ul>
+      <ul></ul>
 
       {isModalOpen && <DashboardCreateModal onClose={handleCloseModal} />}
     </div>
