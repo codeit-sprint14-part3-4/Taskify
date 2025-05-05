49281a70663888dc43f1375b9fd06a542fee5e17
jihoon kwak <kwakjihoon1001@gmail.com>
2025-05-05 20:59:38 +0900

    fix: 불필요한 css 코드 제거

--- src/components/skeleton/SkeletonAttribute.tsx
+++ src/components/skeleton/SkeletonAttribute.tsx
@@ -1,9 +1,9 @@
 export default function SkeletonAttribute() {
   return (
     <div className="bg-white max-w-[62rem] p-[3.2rem]">
-      <div className="text-2xl font-bold mb-6 w-40 h-8 bg-gray-200 rounded animate-pulse"></div>
+      <div className="font-bold mb-6 w-40 h-8 bg-gray-200 rounded animate-pulse"></div>
 
-      <div className="text-lg mb-2 w-32 h-6 bg-gray-200 rounded animate-pulse"></div>
+      <div className="mb-2 w-32 h-6 bg-gray-200 rounded animate-pulse"></div>
 
       <div className="w-full h-12 rounded bg-gray-200 animate-pulse mb-8"></div>
 
